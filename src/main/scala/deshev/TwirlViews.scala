0a295202c63e10deb3038bfda9d587bba5f66aa3
Hristo Deshev <hristo@deshev.com>
2012-02-10 07:59:42 +0200

    Sample text template.

--- src/main/scala/deshev/TwirlViews.scala
+++ src/main/scala/deshev/TwirlViews.scala
@@ -7,4 +7,10 @@ class TwirlViews extends ScalatraServlet {
     val name = params.get("name").getOrElse("Anonymous")
     views.html.index(name = name)
   }
+
+  get("/text") {
+    contentType = "text/plain"
+    val name = params.get("name").getOrElse("Anonymous")
+    views.txt.index(name = name)
+  }
 }
